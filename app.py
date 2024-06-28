b015a684df681097353df1c02ea3996779abb61d
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2024-06-28 18:41:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from scipy.integrate import quad
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+# agrega soporte CORS para todos los endpoints, todos los origenes
 CORS(app)
 
 @app.route("/calculate", methods=['POST'])
-@cross_origin()
 def calculate_volume():
     data = request.get_json()
     a = float(data['start'])
@@ -14,9 +14,7 @@ def calculate_volume():
     area_function = lambda x: eval(data['area'])  # Convertir la función A(x) a una función evaluable
     volume, error = quad(area_function, a, b)
     response = jsonify({'volume': round(volume, 2)})
-    return response, 200, {'Access-Control-Allow-Origin':'*',
-                                'Access-Control-Allow-Methods':'POST',
-                                'Access-Control-Allow-Headers':'content-type'}
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
