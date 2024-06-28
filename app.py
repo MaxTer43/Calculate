bdf099b6e7e58d8c439e4abd445488a2d05ca828
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2024-06-28 17:52:36 -0500

    cors

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
-import flask
 from flask import Flask, request, jsonify
-import numpy as np
 from scipy.integrate import quad
-from starlette.middleware.cors import CORSMiddleware
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
-# Configuración de CORS
-origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
+@app.route("/calculate")
+@cross_origin()
 def calculate_volume():
     data = request.json
     a = float(data['start'])
@@ -25,9 +17,11 @@ def calculate_volume():
     response = jsonify({'volume': round(volume, 2)})
     return response
 
+
 def area_function(x):
     # Aquí debes convertir la función A(x) recibida en una función evaluable
     return eval(request.json['area'])
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
