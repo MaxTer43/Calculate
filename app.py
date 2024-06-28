6bba531d0e8809edaf06d1f819f4845f635cc6f2
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2024-06-28 18:43:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ def calculate_volume():
     b = float(data['end'])
     area_function = lambda x: eval(data['area'])  # Convertir la función A(x) a una función evaluable
     volume, error = quad(area_function, a, b)
-    response = jsonify({'volume': round(volume, 2)})
-    return response
+    return jsonify({'volume': round(volume, 2)}), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
