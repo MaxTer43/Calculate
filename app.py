b87e197ff9517347a880931e40b3318237f0de6b
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2024-06-28 18:34:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def calculate_volume():
     area_function = lambda x: eval(data['area'])  # Convertir la función A(x) a una función evaluable
     volume, error = quad(area_function, a, b)
     response = jsonify({'volume': round(volume, 2)})
-    return response
+    return response, {'Access-Control-Allow-Origin':'*',
+                                'Access-Control-Allow-Methods':'POST',
+                                'Access-Control-Allow-Headers':'content-type'}
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
