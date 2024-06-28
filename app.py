6c2ef51d9c539da5b222a875b0d3809382b71bf8
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2024-06-28 18:00:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,18 @@ from scipy.integrate import quad
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-
-@app.route("/calculate")
+@app.route("/calculate", methods=['POST'])
+@cross_origin()  # Asegura que este endpoint permite solicitudes CORS
 def calculate_volume():
-    data = request.json
+    data = request.get_json()
     a = float(data['start'])
     b = float(data['end'])
     area_function = lambda x: eval(data['area'])  # Convertir la función A(x) a una función evaluable
     volume, error = quad(area_function, a, b)
     response = jsonify({'volume': round(volume, 2)})
     return response
-
-
-def area_function(x):
-    # Aquí debes convertir la función A(x) recibida en una función evaluable
-    return eval(request.json['area'])
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
